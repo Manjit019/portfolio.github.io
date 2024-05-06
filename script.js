400aad93e3058698c7080d9ee69f97503702ade5
Manjit <manjitkumar1725@gmail.com>
2024-05-06 18:39:01 +0530

    nav active color

--- script.js
+++ script.js
@@ -12,16 +12,26 @@ close.addEventListener("click",()=>{
     headerElem.classList.remove("active");
 })
 
+///
+let navContainer=document.querySelector(".right-nav ul");
+let links=navContainer.getElementsByTagName("a");
+for (let i = 0; i < links.length; i++) {
+   links[i].addEventListener("click",function(){
+    let current =document.getElementsByClassName("active-link");
+    current[0].className=current[0].className.replace("active-link","");
+    this.className+=" active-link";
+   });
+}
 
 //scroll to top
 const scrolltopBtn=document.getElementById("scroll-top");
 
 window.onscroll=function(){
    if(document.body.scrollTop>30 || document.documentElement.scrollTop>30){
-    scrolltopBtn.style.bottom="2rem";
+    scrolltopBtn.style.bottom="2.5rem";
    }
    else{
-    scrolltopBtn.style.bottom="-30%";
+    scrolltopBtn.style.bottom="-40%";
    }
 }
 scrolltopBtn.addEventListener('click',()=>{
